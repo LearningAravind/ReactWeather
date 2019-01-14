8b6efb5c29f5978814f7c6d90f4aaef759b869ee
Aravind Anvekar <aravind.anvekar@ziniostech.com>
2019-01-14 14:03:13 +0530

    Rename example page

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 var express = require('express');
 //Create our app
 var app = express();
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
+var port = 3000;
 
-app.use(function (req, res, next){
-  if(req.header['x-forwarded-proto'] === 'http'){
-    next();
-  }else{
+app.use(function (req, res, next) {
+  debugger;
+  if(req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 });
 
 app.use(express.static('public'));
-app.listen(PORT, function(){
-  console.log('Express server is up on port ' + PORT);
+
+app.listen(port, function(){
+  console.log('Express server is up on port ' + port);
 });
